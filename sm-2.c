ad88c3c16b116d7a716b6f09165785c56732b154
Daniel Robins <danr92@gmail.com>
2020-07-08 05:58:36 +1000

    add escape cases for echo and sleep states

--- sm-2.c
+++ sm-2.c
@@ -2,23 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// watch this and implement menu for states
+// https://www.youtube.com/watch?v=3YiPdibiQHA
+
 #define BKSP '\b'
 
 enum states { HOME, ECHO, SLEEP };
 enum states programState = HOME;
 
-
 int startSession();
 int endSession();
-
 
 int main(void) {
   startSession();
   int c;
-  while (c!='q') {
+  while (c != 'q') {
     switch (programState) {
     case HOME: {
-    //   printw("start home case\n");
+      //   printw("start home case\n");
       c = getch();
       if (c == 'E') {
         programState = ECHO;
@@ -27,21 +28,28 @@ int main(void) {
       } else {
         //   putchar('BKSP');
       }
-
       break;
     }
     case ECHO: {
-        
-    //   printw("start echo case\n");
 
-      addch(getch());
+      int echo_char = getch();
 
+      if (echo_char == 'H') {
+        programState = HOME;
+      } else {
+        addch(echo_char);
+      }
       break;
     }
-    case SLEEP:
-
+    case SLEEP: {
+      int echo_char = getch();
+      if (echo_char == 'H') {
+        programState = HOME;
+      } else {
+        // do nothing, dont care about any other echo_char
+      }
       break;
-
+    }
     default:
       // do nothing. why are we here anyway
       break;
@@ -51,13 +59,13 @@ int main(void) {
 }
 
 int startSession() {
-    initscr();
-    noecho();
-    return 0;
+  initscr();
+  noecho();
+  return 0;
 }
 
-int endSession(){
-    echo();
-    endwin();
-    return 0;
+int endSession() {
+  echo();
+  endwin();
+  return 0;
 }
